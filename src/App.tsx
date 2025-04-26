b28bb2c5f28fed194a8137cc3131bcde84416bf6
IlyaShevchenkoSwimmer <shevchil@mail.ru>
2025-04-26 21:42:57 +0300

    created 3d background

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import VolumeBackground from "./components/VolumeBackground/VolumeBackground";
+
+function App() {
+  return (
+    <>
+      <VolumeBackground />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/VolumeBackground/VolumeBackground.tsx
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+import styles from "./styles.module.scss";
+
+export default function VolumeBackground() {
+  const backgroundRef = useRef(null);
+  const backgroundShadowRef = useRef(null);
+
+  function handlePointerMove(event: React.PointerEvent) {
+    const background = backgroundRef.current as unknown as HTMLElement;
+    const backgroundShadow =
+      backgroundShadowRef.current as unknown as HTMLElement;
+    const halfBodyWidth = document.body.offsetWidth / 2;
+    const halfBodyHeight = document.body.offsetHeight / 2;
+    const xPosition = event.clientX;
+    const yPosition = event.clientY;
+    const xAngle = Math.floor((xPosition - halfBodyWidth) / 20);
+    const yAngle = Math.floor((yPosition - halfBodyHeight) / 20);
+    const xShadow = Math.floor((xPosition / (halfBodyWidth * 2)) * 100);
+    const yShadow = Math.floor((yPosition / (halfBodyHeight * 2)) * 100);
+    background.style.transform = `rotateX(${-yAngle}deg) rotateY(${xAngle}deg)`;
+    backgroundShadow.style.background = `radial-gradient(at ${xShadow}% ${yShadow}%, rgba(0, 0, 0, 0) 0, rgba(0, 0, 0, 0.8) 60%)`;
+  }
+
+  return (
+    <div
+      className={styles.background}
+      onPointerMove={handlePointerMove}
+      ref={backgroundRef}
+    >
+      <img src="/VolumeBackground.jpg" />
+      <div
+        className={styles.background__shadow}
+        ref={backgroundShadowRef}
+      ></div>
+    </div>
+  );
+}
